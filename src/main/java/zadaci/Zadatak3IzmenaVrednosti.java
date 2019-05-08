f29f178341204e6c2c548496c94a2c70ffac8a03
Aleksandar Bojanic <s4ca.saca@gmail.com>
2019-05-08 20:39:20 +0200

    Brisanje vrednosti fixes #6

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,11 +1,25 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.SelectArg;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
+
+
+    static Dao<Avion, String> avionDao;
+    static Dao<Roba, String> robaDao;
+
+
 
     public static void main(String[] args) {
 
@@ -15,6 +29,25 @@ public class Zadatak3IzmenaVrednosti {
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
 
+            avionDao = DaoManager.createDao(connectionSource , Avion.class);
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+
+
+
+
+
+            List <Roba> robe =robaDao.queryForEq(Roba.POLJE_OPIS , "Plasticna stolica");
+              Roba roba = robe.get(0);
+
+            roba.setOpis("Drvena stolica");
+            robaDao.update(roba);
+
+
+            List<Roba>  robae =  robaDao.queryForAll();
+           for (Roba r : robae){
+               System.out.println(r);
+           }
 
 
 
--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,11 +1,28 @@
 package zadaci;
 
+import com.j256.ormlite.dao.CloseableIterator;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.ForeignCollection;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.misc.TransactionManager;
+import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 public class Zadatak4BrisanjeVrednosti {
+
+
+
+    static Dao<Avion, String> avionDao;
+    static Dao<Roba, String> robaDao;
+
 
     public static void main(String[] args) {
 
@@ -14,6 +31,9 @@ public class Zadatak4BrisanjeVrednosti {
 
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+
+            avionDao = DaoManager.createDao(connectionSource , Avion.class);
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
 
 
 
