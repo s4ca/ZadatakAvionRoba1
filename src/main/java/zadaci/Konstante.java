6e8d0f5d6809d2541d566f1ec4311017125ca4c4
Aleksandar Bojanic <s4ca.saca@gmail.com>
2019-05-08 18:44:34 +0200

    kreirane klaze za zadatke fixes #1

---
+++ src/main/java/zadaci/Konstante.java
@@ -0,0 +1,6 @@
+package zadaci;
+
+public class Konstante {
+
+    public static final String DATABASE_URL="jdbc:sqlite:restoran.db";
+}
---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,35 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+
+public class Zadatak4BrisanjeVrednosti {
+
+    public static void main(String[] args) {
+
+
+        ConnectionSource connectionSource = null;
+
+        try {
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+           if (connectionSource != null){
+               try {
+                   connectionSource.close();
+               }catch (IOException e){
+                   e.printStackTrace();
+               }
+           }
+        }
+
+
+    }
+}
